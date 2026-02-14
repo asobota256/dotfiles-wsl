47bb561ccd5ffc6395a6f9330d9a774b37b1b443
Andrzej Sobota <asobota256@gmail.com>
2026-02-14 21:26:33 +0100

    Created an install script creating symbolic links to the configuration files in the user's home directory

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+REPO=$(pwd)
+
+# bash
+ln --symbolic --force --verbose \
+  "${REPO}/.bashrc" \
+  "${HOME}/.bashrc"
+ln --symbolic --force --verbose \
+  "${REPO}/.bash_profile" \
+  "${HOME}/.bash_profile"
+
+# git
+ln --symbolic --force --verbose \
+  "${REPO}/.gitconfig" \
+  "${HOME}/.gitconfig"
+
+# vim
+ln --symbolic --force --verbose \
+  "${REPO}/.vimrc" \
+  "${HOME}/.vimrc"
+
+FTPLUGIN="/.vim/after/ftplugin/"
+mkdir --parents --verbose "${HOME}${FTPLUGIN}"
+for file in "${REPO}${FTPLUGIN}"*; do
+  ln --symbolic --force --verbose \
+    "${file}" \
+    "${HOME}${FTPLUGIN}$(basename -- "${file}")"
+done
+
+SYNTAX="/.vim/after/syntax/"
+mkdir --parents --verbose "${HOME}${SYNTAX}"
+for file in "${REPO}${SYNTAX}"*; do
+  ln --symbolic --force --verbose \
+    "${file}" \
+    "${HOME}${SYNTAX}$(basename -- "${file}")"
+done
